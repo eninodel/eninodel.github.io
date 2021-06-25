95b1c23a4de6b2cf1003c21cb3e31ff1d5a93b2c
Edwin Nino Delgado <enino@uw.edu>
2021-06-24 23:08:14 -0700

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import ProfessorRanker from "./ProfessorRanker";
+import SpotifyMix from "./SpotifyMix";
+function Projects() {
+  return (
+    <div id="card">
+      <div>
+        <h1>Projects</h1>
+        <hr />
+      </div>
+      <ProfessorRanker />
+      <SpotifyMix />
+    </div>
+  );
+}
+
+export default Projects;
---
+++ src/components/SpotifyMix.js
@@ -0,0 +1,27 @@
+function SpotifyMix() {
+  return (
+    <div>
+      <h2>Spotify Mix - A Feature I Wish Spotify Had</h2>
+      <p class="bottom-paragraph">
+        I listen to music a lot and my primary source is Spotify. Spotify is
+        great because I can listen to virtually anything I want on demand
+        without pesky ads. As robust as Spotify may be, I found myself wanting
+        something more. I have multiple different playlists for different moods
+        but I was getting bored of them. I wanted to discover more music but I
+        found Spotify's explore pages boring so I decided to create my own way
+        of discovering new music. Using Spotify's API, I wrote a script which
+        takes any playlist of mine and replaces each song with a different
+        random song by the same artist. This creates a new playlist which allows
+        me to easily discover songs by the same artists I already enjoy. This
+        method has lead to me discovering much more music that I like compared
+        to Spotify's explore pages. By completing this project, I increased my
+        knowledge of API's and best practices when using them. Click{" "}
+        <a href="https://github.com/eninodel/Spotify_mix">here</a> to view the
+        repository.
+      </p>
+      <hr />
+    </div>
+  );
+}
+
+export default SpotifyMix;
